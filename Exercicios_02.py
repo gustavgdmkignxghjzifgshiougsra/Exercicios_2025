006d410a6c5637bb86dbb87cf9c9ed6f579a2e76
gustavgdmkignxghjzifgshiougsra <163552564+gustavgdmkignxghjzifgshiougsra@users.noreply.github.com>
2025-05-23 10:40:33 -0300

    Update Exercicios_02.py

--- Exercicios_02.py
+++ Exercicios_02.py
@@ -45,3 +45,49 @@ nota4 = float(input("digite a quarta nota: "))
 media = (nota1 + nota2 + nota3 + nota4) / 4 
 
 print(f" A média das notas é: {media}") 
+
+#Exercio6
+n = int(input("Digite um número: "))
+
+for n in range(1, n + 1):
+ print(f'{n}')
+
+
+#Exercicio7
+palavraDigitada = input('digite uma palavra: ')
+
+
+palavraReserva = palavraDigitada [::-1]
+
+print (f'O contrário da palavra {palavraDigitada} é: {palavraReserva}')
+
+#Exercicio8
+digitenumero = input('Digite um número: ')
+
+numero = int(digitenumero)
+
+resto = numero % 3
+
+if resto == 0:
+    print(numero, 'é múltiplo de 3')
+
+else:
+    print(numero,'não é multiplo de 3')
+
+
+
+
+#Exercicio9
+
+nome1 = input('digite o primeiro nome: ')
+nome2 = input('digite o segundo nome: ')
+nome3 = input('digite o terceiro nome: ')
+
+
+listadenomes = [nome1, nome2, nome3]
+
+listadenomes.sort(reserve=False)
+
+print(f'Os nomes em ordem alfabetica são: {listadenomes}')
+
+
